ea23294eaed46affe14d3a0af00df532a5dfb839
Eric Oliveira <oiericoi@hotmail.com>
2021-11-20 10:49:34 -0300

    I imported db into server.js to create the table in db when starting the server

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const db = require('./database/db');
+const user = require('./models/user');
+
 const port = process.env.PORT || 3000;
 
 async function startServer() {
+
+  await db.sync()
+    .then(() => console.log('Database connected!'))
+    .catch((error) => console.log(error));
 
   const app = express();
 
