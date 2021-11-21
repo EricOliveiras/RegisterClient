e94917a56cdfb9a8f2a102753fdf348ba1fd99ea
Eric Oliveira <oiericoi@hotmail.com>
2021-11-21 17:25:04 -0300

    nothing to commit

--- backend/Routes/routes.js
+++ backend/Routes/routes.js
@@ -4,7 +4,6 @@ const UserController = require('../controllers/userControllers');
 
 const router = express.Router();
 
-
 router.post('/newuser', UserController.createUser);
 router.get('/user', UserController.getUsers);
 
