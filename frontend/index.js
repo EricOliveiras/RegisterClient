77cdfa47befb2e88684045bcaeedc2ca5876dac0
Eric Oliveira <oiericoi@hotmail.com>
2021-11-24 11:54:39 -0300

    I changed some basic things in html and js

--- frontend/index.js
+++ frontend/index.js
@@ -3,34 +3,32 @@ const api = axios.create({
   baseURL: 'http://localhost:8000/user',
 });
 
-
 function getUser() {
   api.get()
-    .then(response => {
-      
-      const render = document.getElementById('render');
-      const btnFind = document.querySelector('.btn-find-all');
-      
-      btnFind.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        const data = response.data;
-
-        data.forEach(user => {
-          const userElement = document.createElement('div');
-          userElement.innerHTML = `
-            <h2>${user.name}</h2>
-            <p>${user.cpf}</p>
-            <p>${user.email}</p>
-            <p>${user.fone}</p>
-          `;
-          render.appendChild(userElement);
-        });
-      });
-    })
-    .catch (error => {
-      console.log(error);
+  .then(response => {
+    const data = response.data;
+    
+    const render = document.getElementById('render');
+    
+    data.forEach(user => {
+      const userElement = document.createElement('div');
+      userElement.innerHTML = `
+      <h2>${user.name}</h2>
+      <p>${user.cpf}</p>
+      <p>${user.email}</p>
+      <p>${user.fone}</p>
+      `;
+      render.appendChild(userElement);
     });
+  })
+  .catch (error => {
+    console.log(error);
+  });
 };
 
-getUser();
+const btnFind = document.querySelector('.btn-find-all');
+
+btnFind.addEventListener('click', (e) => {
+  e.preventDefault();
+  getUser();
+});
